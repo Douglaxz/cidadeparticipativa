4e55d196df14f0ebbac4adff1d34bd9392c840e6
Douglas Amaral <douglaxz@hotmail.com>
2023-03-15 13:00:12 -0300

    primeiro commit

---
+++ models.py
@@ -0,0 +1,29 @@
+from cidadeparticipativa import db
+
+#---------------------------------------------------------------------------------------------------------------------------------
+#TABELA: USUÁRIOS
+#ORIGEM: BANCO DE DADOS
+#---------------------------------------------------------------------------------------------------------------------------------
+class tb_user(db.Model):
+    cod_user = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name_user = db.Column(db.String(50), nullable=False)
+    password_user = db.Column(db.String(50), nullable=False)
+    status_user = db.Column(db.Integer, nullable=False)
+    login_user = db.Column(db.String(50), nullable=False)
+    cod_usertype = db.Column(db.Integer, nullable=False)
+    email_user = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+#---------------------------------------------------------------------------------------------------------------------------------
+#TABELA: TIPO USUÁRIOS
+#ORIGEM: BANCO DE DADOS
+#---------------------------------------------------------------------------------------------------------------------------------
+class tb_usertype(db.Model):
+    cod_usertype = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    desc_usertype = db.Column(db.String(50), nullable=False)
+    status_usertype = db.Column(db.Integer, nullable=False)
+    def __repr__(self):
+        return '<Name %r>' % self.name    
+ 
